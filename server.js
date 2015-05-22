2471da9ffdda0d967c0436997545dfad9d966bcc
tenfortyeight <john@magnusson.gl>
2015-05-22 16:37:10 +0200

    bunch of stuff to persist tweets

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var dbAccess = require('./lib/dbAccess');
+var config = require('./config')();
+
+// hardcode right now
+var mapper = require('./lib/twitterToDiscussionMapper');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4444);
-console.log('Listening on port 4444');
+dbAccess.connectToDb(config.mongodbConnectionString);
+
+// API
+app.post('/api/discussions', function(req, res){
+	var fulldata = "";
+	req.on('data', function(chunk){
+		console.log("data mottaget");
+		fulldata += chunk.toString();
+	});
+
+	req.on('end', function(){
+		var result = JSON.parse(fulldata);
+		console.log("persisting data");
+		var discussion = mapper.map(result);
+		
+		dbAccess.createDiscussion(discussion, function() {
+			// ??
+		});
+		
+		res.send(200);
+	});
+});
+
+app.listen(config.appPort);
+console.log('Listening on port ' + config.appPort);
