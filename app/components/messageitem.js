21b7d262678a29d782f7ec26bc433fd351cf090c
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2015-05-25 23:06:48 +0200

    changed format on message time

--- app/components/messageitem.js
+++ app/components/messageitem.js
@@ -1,12 +1,22 @@
 /** @jsx React.DOM */
 var React = require('react');
+var timeAgo = require('timeago');
 
 module.exports = React.createClass({
 
+	timeago: function (date) {
+		return (timeAgo(date));
+	},
+	timeFormatted: function (date) {
+		return date.toLocaleString();
+	},
 	render: function (){
 		return ( 
 			<section className="messageitem bubble">
-				<div className="header">{this.props.data.author.name} ({this.props.data.author.id}), {this.props.data.timestamp}</div>
+				<div className="header clearfix">
+					<div className="pull-left">{this.props.data.author.name} ({this.props.data.author.id})</div>
+					<div className="pull-right" title={this.timeFormatted(this.props.data.timestamp)}>{this.timeago(this.props.data.timestamp)}</div>
+				</div>
 				<div className="body">{this.props.data.content}</div>
 			</section>
 		);
